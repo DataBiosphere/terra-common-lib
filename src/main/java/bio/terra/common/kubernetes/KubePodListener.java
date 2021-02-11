78a4a8cfe846c201d31e67cc279db0c91d1b6330
tnadler <tnadler@broadinstitute.org>
2021-02-10 21:13:18 -0500

    [PF-278] First pass at Kubernetes common library (#15)
    * First pass at Kubernetes common library
    
    * Remove unused variable; add documentation
    
    * Make pod listener package private
    Consolidate shutdown state and stopPodListener
    Throw error if file cannot be read
    
    * Updates from review
    
    Co-authored-by: ddietterich <ddietterich@verily.com>

---
+++ src/main/java/bio/terra/common/kubernetes/KubePodListener.java
@@ -0,0 +1,190 @@
+package bio.terra.common.kubernetes;
+
+import bio.terra.stairway.Stairway;
+import bio.terra.stairway.exception.DatabaseOperationException;
+import bio.terra.stairway.exception.StairwayExecutionException;
+import com.google.common.reflect.TypeToken;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Namespace;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.Watch;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Kubernetes Pod Listener listens for pod creation and deletion events within a namespace. When
+ * a pod is deleted, the listener tells Stairway to perform recovery on flights owned by the
+ * Stairway with that pod name. It works so long as the pod name is used as the stairway instance
+ * name when the Stairway object is created.
+ *
+ * <p>Typically, there are different types of pods in a namespace. We only want to listen to pods
+ * that are making a Stairway cluster. We rely on consistent pod naming to filter for those pods.
+ * The listener accepts a string and only attends to pods whose names contain that string.
+ *
+ * <p>Experience shows that the watch on the Kubernetes pod list is not stable. It drops and is
+ * recreated in a retry loop. Happily, recreating the watch causes Kubernetes to reiterate all of
+ * the current pods, so the listener does not miss state changes.
+ */
+@SuppressFBWarnings(
+    value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
+    justification = "Spotbugs doesn't understand resource try construct")
+class KubePodListener implements Runnable {
+
+  enum PodState {
+    RUNNING,
+    DELETED
+  }
+
+  private static final int WATCH_RETRIES = 10;
+  private static final int WATCH_INITIAL_WAIT = 5;
+  private static final int WATCH_MAX_WAIT = 30;
+
+  private final Logger logger = LoggerFactory.getLogger(KubePodListener.class);
+  private final KubeService kubeService;
+  private final String namespace;
+  private final String podNameFilter;
+  private final Stairway stairway;
+  private Exception exception;
+  private final Map<String, PodState> podMap;
+
+  /**
+   * Setup the listener configuration.
+   *
+   * @param kubeService The parent service, used to check the shutdown state
+   * @param stairway The Stairway instance to use for recovering deleted pods
+   * @param namespace Kubernetes namespace to listen in
+   * @param podNameFilter Only pods with names containing this string are attended to by the
+   *     listener
+   */
+  KubePodListener(
+      KubeService kubeService, Stairway stairway, String namespace, String podNameFilter) {
+    this.kubeService = kubeService;
+    this.namespace = namespace;
+    this.podNameFilter = podNameFilter;
+    this.stairway = stairway;
+    this.exception = null;
+
+    // The map must be concurrent, since the computation of active pods may be done on different
+    // threads than the pod listener accesses.
+    podMap = new ConcurrentHashMap<>();
+  }
+
+  @Override
+  public void run() {
+    logger.info("KubePodListener starting");
+
+    exception = null;
+    int consecutiveRetryCount = 0;
+    int retryWait = WATCH_INITIAL_WAIT;
+    while (true) {
+      try {
+        ApiClient client = Config.defaultClient();
+        // infinite timeout
+        OkHttpClient httpClient =
+            client.getHttpClient().newBuilder().readTimeout(0, TimeUnit.SECONDS).build();
+        client.setHttpClient(httpClient);
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        try (Watch<V1Namespace> watch =
+            Watch.createWatch(
+                client,
+                api.listNamespacedPodCall(
+                    namespace, null, null, null, null, null, 5, null, null, Boolean.TRUE, null),
+                new TypeToken<Watch.Response<V1Namespace>>() {}.getType())) {
+          for (Watch.Response<V1Namespace> item : watch) {
+            // If we are shutting down, we stop watching
+            if (kubeService.isShutdown()) {
+              return;
+            }
+            // Reset retry if the watch worked
+            consecutiveRetryCount = 0;
+            retryWait = WATCH_INITIAL_WAIT;
+
+            String operation = item.type;
+            String podName =
+                (item.object.getMetadata() != null)
+                    ? item.object.getMetadata().getName()
+                    : org.apache.commons.lang3.StringUtils.EMPTY;
+            logger.info(String.format("%s : %s", operation, podName));
+
+            if (StringUtils.contains(podName, podNameFilter)) {
+              if (StringUtils.equals(operation, "ADDED")) {
+                logger.info("Added api pod: " + podName);
+                podMap.put(podName, PodState.RUNNING);
+              } else if (StringUtils.equals(operation, "DELETED")) {
+                try {
+                  logger.info("Attempting clean up of deleted stairway instance: " + podName);
+                  stairway.recoverStairway(podName);
+                  PodState deletedPodValue = podMap.get(podName);
+                  if (deletedPodValue == PodState.RUNNING) {
+                    logger.info("Deleted api pod: " + podName);
+                    podMap.put(podName, PodState.DELETED);
+                  }
+                } catch (DatabaseOperationException | StairwayExecutionException ex) {
+                  logger.error("Stairway recoverStairway failed to recover pod: " + podName, ex);
+                } catch (InterruptedException ex) {
+                  logger.info("KubePodListener interrupted - exiting", ex);
+                  exception = ex;
+                  return;
+                }
+              }
+            }
+          }
+        }
+      } catch (RuntimeException | ApiException | IOException ex) {
+        exception = ex;
+        logger.info("KubePodListener caught exception: " + exception);
+      }
+
+      // Exponential backoff retry after an exception from the watch
+      // Now and then, the watch gets a network timeout. This loop restarts it. We don't want a
+      // hard failure to just continue to loop, so if it hits the error WATCH_RETRIES times in a
+      // row, we bail out of the listener.
+      if (consecutiveRetryCount >= WATCH_RETRIES) {
+        logger.error("KubePodListener exiting - exceeded max consecutive retries", exception);
+        return;
+      }
+      consecutiveRetryCount++;
+
+      try {
+        logger.info("KubePodListener retry wait seconds: " + retryWait);
+        TimeUnit.SECONDS.sleep(retryWait);
+        retryWait = Math.min(retryWait + retryWait, WATCH_MAX_WAIT);
+      } catch (InterruptedException ex) {
+        logger.info("KubePodListener exiting - interruped while retrying");
+        return;
+      }
+      logger.info("KubePodListener consecutive retry: " + consecutiveRetryCount);
+    }
+  }
+
+  Exception getException() {
+    return exception;
+  }
+
+  Map<String, PodState> getPodMap() {
+    return podMap;
+  }
+
+  int getActivePodCount() {
+    int count = 0;
+    for (PodState podState : podMap.values()) {
+      if (podState == PodState.RUNNING) {
+        count++;
+      }
+    }
+    logger.info("KubePodListener ActivePodCount: {} of {} pods active", count, podMap.size());
+    return count;
+  }
+}
---
+++ src/main/java/bio/terra/common/kubernetes/KubeService.java
@@ -0,0 +1,190 @@
+package bio.terra.common.kubernetes;
+
+import bio.terra.common.kubernetes.exception.KubeApiException;
+import bio.terra.stairway.Stairway;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * KubeService provides access to a given Kubernetes environment.
+ *
+ * <p>The most common use in McTerra is for services that use Stairway to be able to recover flights
+ * that fail when a pod goes away. The current convention is to use the pod name as the stairway
+ * name. Then the service can compare the list of stairways it thinks should be running, compare
+ * with list of active pods, and recover anything that is no longer active.
+ */
+@SuppressFBWarnings(
+    value = "DMI_HARDCODED_ABSOLUTE_FILENAME",
+    justification = "The K8s namespace file is a valid absolute filename")
+public class KubeService {
+  private static final Logger logger = LoggerFactory.getLogger(KubeService.class);
+
+  // Location in the container where Kubernetes stores service account and
+  // namespace information. Kubernetes mints a service account for the container (pod?)
+  // that can be used to make requests of Kubernetes.
+  static final String KUBE_DIR = "/var/run/secrets/kubernetes.io/serviceaccount";
+  static final String KUBE_NAMESPACE_FILE = KUBE_DIR + "/namespace";
+
+  private final String podName;
+  private final boolean inKubernetes;
+  private final String podNameFilter;
+  private final String namespace;
+  private final AtomicBoolean isShutdown;
+
+  private KubePodListener podListener;
+  private Thread podListenerThread;
+
+  /**
+   * @param podName Name of the Kubernetes pod we are running in. If we are not in a pod, this
+   *     defaults to a constant string in application properties. We cannot easily find our own pod
+   *     name, so components must plumb this in using settings in the helm charts.
+   * @param inKubernetes Used to denote that we are running in the Kubernetes environment. The
+   *     service can be called and provides reasonable answers when inKubernetes is false.
+   * @param podNameFilter Filter to apply to pod names returned by Kubernetes API to know which pods
+   *     we care about.
+   */
+  public KubeService(String podName, boolean inKubernetes, String podNameFilter) {
+    this.podName = podName;
+    this.inKubernetes = inKubernetes;
+    this.podNameFilter = podNameFilter;
+    if (inKubernetes) {
+      this.namespace = readFileIntoString(KUBE_NAMESPACE_FILE);
+    } else {
+      this.namespace = "nonamespace";
+    }
+    this.isShutdown = new AtomicBoolean(false);
+
+    logger.info(
+        "Kubernetes configuration: inKube: "
+            + inKubernetes
+            + "; namespace: "
+            + namespace
+            + "; podName: "
+            + podName);
+  }
+
+  /**
+   * Get the set of filtered pods from Kubernetes. It is returned as a set to make probing for
+   * specific names easy.
+   *
+   * @return set of pod names containing the podNameFilter string; null if not in kubernetes
+   */
+  public Set<String> getPodSet() {
+    Set<String> pods = new HashSet<>();
+    if (!inKubernetes) {
+      return pods;
+    }
+
+    try {
+      CoreV1Api api = makeCoreApi();
+      V1PodList list =
+          api.listNamespacedPod(namespace, null, null, null, null, null, null, null, null, null);
+      for (V1Pod item : list.getItems()) {
+        if (item.getMetadata() != null) {
+          String podName = item.getMetadata().getName();
+          if (StringUtils.contains(podName, podNameFilter)) {
+            pods.add(podName);
+          }
+        }
+      }
+      return pods;
+    } catch (ApiException ex) {
+      throw new KubeApiException("Error listing pods", ex);
+    }
+  }
+
+  /**
+   * Launch the {@link KubePodListener} thread.
+   *
+   * @param stairway the Stairway instance passed to the pod listener for Stairway recovery
+   */
+  public void startPodListener(Stairway stairway) {
+    if (inKubernetes) {
+      podListener = new KubePodListener(this, stairway, namespace, podNameFilter);
+      podListenerThread = new Thread(podListener);
+      podListenerThread.start();
+    }
+  }
+
+  public int getActivePodCount() {
+    if (podListener != null) {
+      return podListener.getActivePodCount();
+    }
+    int defaultPodCount = 1;
+    logger.info("KubeService ActivePodCount - default val: {}", defaultPodCount);
+    return defaultPodCount;
+  }
+
+  /**
+   * Stop pod listener flips the isShutdown flag so we don't do it more than once. It tries to join
+   * the pod listener thread.
+   *
+   * @param timeUnit time unit to wait for the listener thread to stop
+   * @param joinWait time in the time unit to wait
+   * @return true if we joined the listener thread; false if that timed out
+   */
+  public boolean stopPodListener(TimeUnit timeUnit, long joinWait) {
+    if (!isShutdown.get() && inKubernetes) {
+      isShutdown.set(true);
+      // stop the pod listener thread
+      podListenerThread.interrupt();
+      long waitMillis = timeUnit.toMillis(joinWait);
+      try {
+        podListenerThread.join(waitMillis);
+      } catch (InterruptedException ex) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public String getPodName() {
+    return podName;
+  }
+
+  public String getNamespace() {
+    return namespace;
+  }
+
+  // Method for pod listener to test shutdown state
+  boolean isShutdown() {
+    return isShutdown.get();
+  }
+
+  private CoreV1Api makeCoreApi() {
+    try {
+      ApiClient client = ClientBuilder.cluster().build();
+      Configuration.setDefaultApiClient(client);
+      return new CoreV1Api();
+    } catch (IOException ex) {
+      throw new KubeApiException("Error making core API", ex);
+    }
+  }
+
+  private String readFileIntoString(String path) {
+    try {
+      byte[] encoded = Files.readAllBytes(Paths.get(path));
+      return new String(encoded, StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new KubeApiException("Failed to read file: " + path, e);
+    }
+  }
+}
