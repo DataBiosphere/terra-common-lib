0f95d122db3447e45f482854ee37b346d4aea4f8
yonghao <yyhao1@gmail.com>
2021-03-18 11:31:13 -0400

    data source

--- src/main/java/bio/terra/common/db/PrimaryDatabaseProperties.java
+++ src/main/java/bio/terra/common/db/PrimaryDatabaseProperties.java
@@ -2,5 +2,6 @@ package bio.terra.common.db;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/** Pre-defined {@link BaseDatabaseProperties} that configs one database configuration properties. */
 @ConfigurationProperties(prefix = "terra.common.db")
 public class PrimaryDatabaseProperties extends BaseDatabaseProperties {}
