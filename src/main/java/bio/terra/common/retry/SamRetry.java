4ae715aacde01939d1ea3d226e9c33a2a8519f5a
Tova Nadler <tnadler@broadinstitute.org>
2021-04-08 16:26:24 -0400

    [PF-388] Rewire SamRetry to throw a custom SamApiException type that keeps track of the HTTP Status Code. This exception is not derived from ErrorReportException so that it is a checked exception.

--- src/main/java/bio/terra/common/retry/SamRetry.java
+++ src/main/java/bio/terra/common/retry/SamRetry.java
@@ -2,23 +2,14 @@ package bio.terra.common.retry;
 
 import static java.time.Instant.now;
 
-import bio.terra.common.exception.BadRequestException;
-import bio.terra.common.exception.ConflictException;
-import bio.terra.common.exception.ErrorReportException;
-import bio.terra.common.exception.ForbiddenException;
 import bio.terra.common.exception.InternalServerErrorException;
-import bio.terra.common.exception.NotFoundException;
-import bio.terra.common.exception.UnauthorizedException;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import bio.terra.common.exception.SamApiException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.http.HttpStatusCodes;
 import com.google.common.annotations.VisibleForTesting;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
-import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsde.workbench.client.sam.ApiException;
-import org.broadinstitute.dsde.workbench.client.sam.model.ErrorReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -26,6 +17,8 @@ import org.springframework.http.HttpStatus;
 /**
  * SamRetry encapsulates logic needed for retrying Sam API calls. All constants are hard-coded, as
  * Sam failures are generally just short periods of down-time.
+ * SamRetry always throws a SamApiException, which extracts the error message from Sam (which can be
+ * found in two different places) and stores the HTTP Status from Sam.
  */
 public class SamRetry {
   private static Logger logger = LoggerFactory.getLogger(SamRetry.class);
@@ -47,8 +40,7 @@ public class SamRetry {
     this.retryDuration = INITIAL_WAIT;
   }
 
-  @VisibleForTesting
-  SamRetry(Duration timeout) {
+  protected SamRetry(Duration timeout) {
     this.operationTimeout = now().plus(timeout);
     this.retryDuration = INITIAL_WAIT;
   }
@@ -63,42 +55,39 @@ public class SamRetry {
     R apply() throws ApiException, InterruptedException;
   }
 
-  public static <T> T retry(SamFunction<T> function) throws InterruptedException {
+  public static <T> T retry(SamFunction<T> function) throws SamApiException {
     SamRetry samRetry = new SamRetry();
     return samRetry.perform(function);
   }
 
   // Version of the retry function for testing so that we can override timeout
-  @VisibleForTesting
-  public static <T> T retry(SamFunction<T> function, Duration timeout) throws InterruptedException {
+  protected static <T> T retry(SamFunction<T> function, Duration timeout) throws SamApiException {
     SamRetry samRetry = new SamRetry(timeout);
     return samRetry.perform(function);
   }
 
-  public static void retry(SamVoidFunction function) throws InterruptedException {
+  public static void retry(SamVoidFunction function) throws SamApiException {
     SamRetry samRetry = new SamRetry();
     samRetry.performVoid(function);
   }
 
   // Version of the retry function for testing so that we can override timeout
-  @VisibleForTesting
-  public static void retry(SamVoidFunction function, Duration timeout) throws InterruptedException {
+  protected static void retry(SamVoidFunction function, Duration timeout) throws SamApiException {
     SamRetry samRetry = new SamRetry(timeout);
     samRetry.performVoid(function);
   }
 
-  private <T> T perform(SamFunction<T> function) throws InterruptedException {
+  private <T> T perform(SamFunction<T> function) throws SamApiException {
     while (true) {
       try {
         return function.apply();
       } catch (ApiException ex) {
-        ErrorReportException errorReportException = convertSAMExToCommonEx(ex);
-        if (isRetryable(errorReportException)) {
+        SamApiException samApiException = SamApiException.createSamApiException(ex);
+        if (isRetryable(samApiException)) {
           logger.info("SamRetry: caught retry-able exception: ", ex);
-          sleepOrTimeoutBeforeRetrying(errorReportException);
-          continue;
+          sleepOrTimeoutBeforeRetrying(samApiException);
         } else {
-          throw errorReportException;
+          throw samApiException;
         }
       } catch (Exception ex) {
         throw new InternalServerErrorException("Unexpected exception type: " + ex.toString(), ex);
@@ -106,11 +95,11 @@ public class SamRetry {
     }
   }
 
-  private boolean isRetryable(ErrorReportException errorReportException) {
-    return (errorReportException.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR);
+  private boolean isRetryable(SamApiException samApiException) {
+    return (samApiException.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  private void performVoid(SamVoidFunction function) throws InterruptedException {
+  private void performVoid(SamVoidFunction function) throws SamApiException {
     perform(
         () -> {
           function.apply();
@@ -118,76 +107,30 @@ public class SamRetry {
         });
   }
 
-  private void sleepOrTimeoutBeforeRetrying(ErrorReportException errorReportException)
-      throws InterruptedException {
-    // if our operation timeout will happen before we wake up from our next retry
-    // sleep, then we give up and re-throw.
+  /**
+   * Given an exception from Sam, either timeout and rethrow the error from Sam
+   * or sleep for retryDuration. If the thread times out while sleeping, throw the initial
+   * exception.
+   * @param samApiException The error Sam threw
+   * @throws SamApiException
+   */
+  private void sleepOrTimeoutBeforeRetrying(SamApiException samApiException)
+      throws SamApiException {
     if (operationTimeout.minus(retryDuration).isBefore(now())) {
       logger.error("SamRetry: operation timed out after " + operationTimeout.toString());
-      throw errorReportException;
+      // If we timed out, throw the error from Sam that caused us to need to retry.
+      throw samApiException;
     }
     logger.info("SamRetry: sleeping " + retryDuration.getSeconds() + " seconds");
-    TimeUnit.SECONDS.sleep(retryDuration.getSeconds());
-
+    try {
+      TimeUnit.SECONDS.sleep(retryDuration.getSeconds());
+    } catch (InterruptedException ex) {
+      logger.error("SamRetry: thread interrupted while sleeping: ", ex);
+      throw samApiException;
+    }
     retryDuration = retryDuration.plus(INITIAL_WAIT);
     if (retryDuration.compareTo(MAXIMUM_WAIT) > 0) {
       retryDuration = MAXIMUM_WAIT;
-    }
-  }
-
-  /**
-   * Converts a SAM-specific ApiException to the common API exception, based on the HTTP status
-   * code.
-   */
-  private static ErrorReportException convertSAMExToCommonEx(final ApiException samEx) {
-    logger.warn("SAM client exception code: {}", samEx.getCode());
-    logger.warn("SAM client exception message: {}", samEx.getMessage());
-    logger.warn("SAM client exception details: {}", samEx.getResponseBody());
-
-    // Sometimes the sam message is buried one level down inside of the error report object.
-    // If we find an empty message then we try to deserialize the error report and use that message.
-    String message = samEx.getMessage();
-    if (StringUtils.isEmpty(message)) {
-      try {
-        ErrorReport errorReport =
-            objectMapper.readValue(samEx.getResponseBody(), ErrorReport.class);
-        message = errorReport.getMessage();
-      } catch (JsonProcessingException ex) {
-        logger.debug("Unable to deserialize sam exception response body");
-      }
-    }
-
-    switch (samEx.getCode()) {
-      case HttpStatusCodes.STATUS_CODE_BAD_REQUEST:
-        {
-          return new BadRequestException(message, samEx);
-        }
-      case HttpStatusCodes.STATUS_CODE_UNAUTHORIZED:
-        {
-          return new UnauthorizedException(message, samEx);
-        }
-      case HttpStatusCodes.STATUS_CODE_FORBIDDEN:
-        {
-          return new ForbiddenException(message, samEx);
-        }
-      case HttpStatusCodes.STATUS_CODE_NOT_FOUND:
-        {
-          return new NotFoundException(message, samEx);
-        }
-      case HttpStatusCodes.STATUS_CODE_CONFLICT:
-        {
-          return new ConflictException(message, samEx);
-        }
-      case HttpStatusCodes.STATUS_CODE_SERVER_ERROR:
-        {
-          return new InternalServerErrorException(message, samEx);
-        }
-        // note that SAM does not use a 501 NOT_IMPLEMENTED status code, so that case is skipped
-        // here
-      default:
-        {
-          return new InternalServerErrorException(message, samEx);
-        }
     }
   }
 }
--- src/test/java/bio/terra/common/retry/SamRetryTest.java
+++ src/test/java/bio/terra/common/retry/SamRetryTest.java
@@ -3,9 +3,11 @@ package bio.terra.common.retry;
 import static org.junit.Assert.assertThrows;
 
 import bio.terra.common.exception.InternalServerErrorException;
+import bio.terra.common.exception.SamApiException;
 import com.google.api.client.http.HttpStatusCodes;
 import java.time.Duration;
 import org.broadinstitute.dsde.workbench.client.sam.ApiException;
+import org.checkerframework.dataflow.qual.TerminatesExecution;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -20,15 +22,37 @@ public class SamRetryTest {
     count = 0;
   }
 
+  @Test
   public void testRetryTimeout() throws Exception {
     assertThrows(
-        InternalServerErrorException.class,
+        SamApiException.class,
         () -> SamRetry.retry(() -> testRetryFinishInner(100), Duration.ofSeconds(10)));
   }
 
   @Test
   public void testRetryFinish() throws Exception {
     SamRetry.retry(() -> testRetryFinishInner(2));
+  }
+
+  @Test
+  public void testRetrySamError() throws Exception {
+    assertThrows(
+        SamApiException.class,
+        () -> SamRetry.retry(() -> testRetryThrows()));
+  }
+
+  @Test
+  public void testRetryVoidTimeout() throws Exception {
+    assertThrows(
+        // The SamRetry class will catch timeouts and throw a SamApiException that wraps the initial
+        // failure from Sam that caused us to need to retry.
+        SamApiException.class,
+        () -> SamRetry.retry(() -> testRetryVoidFinishInner(100), Duration.ofSeconds(10)));
+  }
+
+  @Test
+  public void testRetryVoidFinish() throws Exception {
+    SamRetry.retry(() -> testRetryVoidFinishInner(2));
   }
 
   private boolean testRetryFinishInner(int failCount) throws ApiException {
@@ -39,21 +63,14 @@ public class SamRetryTest {
     return true;
   }
 
-  public void testRetryVoidTimeout() throws Exception {
-    assertThrows(
-        InternalServerErrorException.class,
-        () -> SamRetry.retry(() -> testRetryVoidFinishInner(100), Duration.ofSeconds(10)));
-  }
-
-  @Test
-  public void testRetryVoidFinish() throws Exception {
-    SamRetry.retry(() -> testRetryVoidFinishInner(2));
-  }
-
   private void testRetryVoidFinishInner(int failCount) throws ApiException {
     if (count < failCount) {
       count++;
       throw new ApiException(HttpStatusCodes.STATUS_CODE_SERVER_ERROR, "testing");
     }
+  }
+
+  private boolean testRetryThrows() throws ApiException {
+    throw new ApiException(HttpStatusCodes.STATUS_CODE_NOT_FOUND, "testing");
   }
 }
