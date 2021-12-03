fb4e85ce3cffc57aa630b2a9b3afdc0335e7b1db
Jack Warren <jack@jackwarren.info>
2021-12-03 13:23:45 -0500

    Format

--- src/main/java/bio/terra/common/metrics/MetricsComponent.java
+++ src/main/java/bio/terra/common/metrics/MetricsComponent.java
@@ -2,56 +2,56 @@ package bio.terra.common.metrics;
 
 import io.opencensus.exporter.stats.prometheus.PrometheusStatsCollector;
 import io.prometheus.client.exporter.HTTPServer;
+import java.io.IOException;
+import java.util.Objects;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-import java.util.Objects;
-
-/**
- * A Spring Component that exposes any OpenCensus metrics via a {@link HTTPServer}.
- */
+/** A Spring Component that exposes any OpenCensus metrics via a {@link HTTPServer}. */
 @Component
 public class MetricsComponent {
-    private final Logger logger = LoggerFactory.getLogger(MetricsComponent.class);
-    private final MetricsProperties metricsProperties;
+  private final Logger logger = LoggerFactory.getLogger(MetricsComponent.class);
+  private final MetricsProperties metricsProperties;
 
-    private HTTPServer prometheusServer;
+  private HTTPServer prometheusServer;
 
-    @Autowired
-    public MetricsComponent(MetricsProperties metricsProperties) {
-        this.metricsProperties = metricsProperties;
+  @Autowired
+  public MetricsComponent(MetricsProperties metricsProperties) {
+    this.metricsProperties = metricsProperties;
+  }
+
+  @PostConstruct
+  private void startEndpointServer() {
+    logger.info(
+        "Prometheus metrics endpoint enabled: {}", metricsProperties.isPrometheusEndpointEnabled());
+    if (!metricsProperties.isPrometheusEndpointEnabled()) {
+      return;
     }
-
-    @PostConstruct
-    private void startEndpointServer() {
-        logger.info("Prometheus metrics endpoint enabled: {}", metricsProperties.isPrometheusEndpointEnabled());
-        if (!metricsProperties.isPrometheusEndpointEnabled()) {
-            return;
-        }
-        try {
-            PrometheusStatsCollector.createAndRegister();
-        } catch (IllegalArgumentException e) {
-            logger.error("OpenCensus Prometheus Collector already registered.", e);
-        }
-        try {
-            prometheusServer = new HTTPServer(metricsProperties.getPrometheusEndpointPort());
-            logger.info("Prometheus endpoint server started. Port: {}", metricsProperties.getPrometheusEndpointPort());
-        } catch (IOException e) {
-            logger.error("Prometheus endpoint server error on startup.", e);
-        }
+    try {
+      PrometheusStatsCollector.createAndRegister();
+    } catch (IllegalArgumentException e) {
+      logger.error("OpenCensus Prometheus Collector already registered.", e);
     }
-
-    @PreDestroy
-    private void stopEndpointServer() {
-        if (Objects.isNull(prometheusServer)) {
-            return;
-        }
-        prometheusServer.close();
-        logger.info("Prometheus server stopped.");
+    try {
+      prometheusServer = new HTTPServer(metricsProperties.getPrometheusEndpointPort());
+      logger.info(
+          "Prometheus endpoint server started. Port: {}",
+          metricsProperties.getPrometheusEndpointPort());
+    } catch (IOException e) {
+      logger.error("Prometheus endpoint server error on startup.", e);
     }
+  }
+
+  @PreDestroy
+  private void stopEndpointServer() {
+    if (Objects.isNull(prometheusServer)) {
+      return;
+    }
+    prometheusServer.close();
+    logger.info("Prometheus server stopped.");
+  }
 }
