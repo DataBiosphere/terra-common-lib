c24966f6dc9e3106dfec3cfb223b74684ab042c4
Dan Dietterich <ddietterich@verily.com>
2022-09-20 09:23:23 -0400

    [PF-1959] Add DataSourceManager; refactor DataSourceInitializer (#84)
    * Add DataSourceManager; refactor DataSourceInitializer
    
    * spotless - but I did a build, why didn't that run spotlessApply?
    
    Co-authored-by: bumptagbot <robot@terra.team>

--- src/main/java/bio/terra/common/db/DataSourceInitializer.java
+++ src/main/java/bio/terra/common/db/DataSourceInitializer.java
@@ -7,13 +7,31 @@ import org.apache.commons.pool2.ObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
-/** Utility class to create {@link DataSource}. */
+/**
+ * Utility class to create {@link DataSource}.
+ *
+ * <p>Prefer using {@DataSourceManager} instead of this code so that connection pools are closed
+ * when the Spring application context is deleted.
+ */
 public class DataSourceInitializer {
   private DataSourceInitializer() {}
   ;
 
   /** Create a {@link DataSource} by providing a {@link BaseDatabaseProperties}. */
   public static DataSource initializeDataSource(BaseDatabaseProperties baseDatabaseProperties) {
+    ObjectPool<PoolableConnection> connectionPool = makeConnectionPool(baseDatabaseProperties);
+    return new PoolingDataSource<>(connectionPool);
+  }
+
+  /**
+   * This method is shared by this class and the DataSourceManager so we only have one
+   * implementation of creating the connection pool
+   *
+   * @param baseDatabaseProperties common database properties
+   * @return connection pool
+   */
+  public static ObjectPool<PoolableConnection> makeConnectionPool(
+      BaseDatabaseProperties baseDatabaseProperties) {
     Properties props = new Properties();
     props.setProperty("user", baseDatabaseProperties.getUsername());
     props.setProperty("password", baseDatabaseProperties.getPassword());
@@ -32,7 +50,6 @@ public class DataSourceInitializer {
         new GenericObjectPool<>(poolableConnectionFactory, config);
 
     poolableConnectionFactory.setPool(connectionPool);
-
-    return new PoolingDataSource<>(connectionPool);
+    return connectionPool;
   }
 }
---
+++ src/main/java/bio/terra/common/db/DataSourceManager.java
@@ -0,0 +1,44 @@
+package bio.terra.common.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.ObjectPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class initializes data source pools just like {@link DataSourceInitializer}, but it
+ * remembers the pools that it creates. It uses a @PreDestroy method to close the pools when the
+ * Spring application context is closed.
+ *
+ * <p>Using this class is important when running JUnit tests that use @DirtiesContext directives.
+ * Without this extra step, when each dirty context is deleted, connections in its pools are
+ * orphaned. Use of the class has no effect in non-test environments.
+ */
+@Component
+public class DataSourceManager {
+  private static final Logger logger = LoggerFactory.getLogger(DataSourceManager.class);
+  private List<ObjectPool<PoolableConnection>> connectionPools = new ArrayList<>();
+
+  @PreDestroy
+  public void destroy() {
+    logger.info("Closing all connection pools");
+    for (var pool : connectionPools) {
+      logger.info("Pool active {} idle {}", pool.getNumActive(), pool.getNumIdle());
+      pool.close();
+    }
+    connectionPools = new ArrayList<>();
+  }
+
+  public DataSource initializeDataSource(BaseDatabaseProperties baseDatabaseProperties) {
+    ObjectPool<PoolableConnection> connectionPool =
+        DataSourceInitializer.makeConnectionPool(baseDatabaseProperties);
+    connectionPools.add(connectionPool);
+    return new PoolingDataSource<>(connectionPool);
+  }
+}
