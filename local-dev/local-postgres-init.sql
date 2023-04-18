ebc065d892b0794c3fa7672eda44fc063c631943
Dan Dietterich <ddietterich@verily.com>
2023-04-18 15:42:23 -0400

    Move to new version; fix postgres init script (#106)
    Co-authored-by: bumptagbot <robot@terra.team>

--- local-dev/local-postgres-init.sql
+++ local-dev/local-postgres-init.sql
@@ -2,3 +2,6 @@ CREATE DATABASE testdb;
 CREATE ROLE dbuser WITH LOGIN ENCRYPTED PASSWORD 'dbpwd';
 CREATE DATABASE tclstairway;
 CREATE ROLE tclstairwayuser WITH LOGIN ENCRYPTED PASSWORD 'tclstairwaypwd';
+GRANT ALL ON DATABASE tclstairway TO tclstairwayuser WITH GRANT OPTION;
+\c tclstairway
+GRANT ALL ON SCHEMA public TO tclstairwayuser WITH GRANT OPTION;
