bcd3258f36b91e09b256af7840ce40d9a16c18b7
Douglas Voet <dvoet@users.noreply.github.com>
2023-10-25 12:52:32 -0400

    opentelemetry prometheus metrics

--- src/main/java/bio/terra/common/prometheus/PrometheusComponent.java
+++ src/main/java/bio/terra/common/prometheus/PrometheusComponent.java
@@ -1,56 +1,36 @@
 package bio.terra.common.prometheus;
 
-import io.opencensus.exporter.stats.prometheus.PrometheusStatsCollector;
+import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetry;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.resources.Resource;
 import io.prometheus.client.exporter.HTTPServer;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import java.io.IOException;
-import java.util.Objects;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-/** A Spring Component that exposes any OpenCensus metrics via a Prometheus {@link HTTPServer}. */
-@Component
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** Spring Beans that exposes any OpenTelemetry metrics via a Prometheus {@link HTTPServer}. */
+@Configuration
+@EnableOpenTelemetry
 public class PrometheusComponent {
-  private static final Logger logger = LoggerFactory.getLogger(PrometheusComponent.class);
-  private final PrometheusProperties prometheusProperties;
-
-  private HTTPServer prometheusServer;
-
-  @Autowired
-  public PrometheusComponent(PrometheusProperties prometheusProperties) {
-    this.prometheusProperties = prometheusProperties;
+  @Bean(destroyMethod = "close")
+  @ConditionalOnProperty(
+      name = "terra.common.prometheus.endpointEnabled",
+      havingValue = "true",
+      matchIfMissing = true)
+  public PrometheusHttpServer prometheusHttpServer(PrometheusProperties prometheusProperties) {
+    return PrometheusHttpServer.builder().setPort(prometheusProperties.getEndpointPort()).build();
   }
 
-  @PostConstruct
-  private void startEndpointServer() {
-    logger.info(
-        "Prometheus metrics endpoint enabled: {}", prometheusProperties.isEndpointEnabled());
-    if (!prometheusProperties.isEndpointEnabled()) {
-      return;
-    }
-    try {
-      PrometheusStatsCollector.createAndRegister();
-    } catch (IllegalArgumentException e) {
-      logger.error("OpenCensus Prometheus Collector already registered.", e);
-    }
-    try {
-      prometheusServer = new HTTPServer(prometheusProperties.getEndpointPort());
-      logger.info(
-          "Prometheus endpoint server started. Port: {}", prometheusProperties.getEndpointPort());
-    } catch (IOException e) {
-      logger.error("Prometheus endpoint server error on startup.", e);
-    }
-  }
-
-  @PreDestroy
-  private void stopEndpointServer() {
-    if (Objects.isNull(prometheusServer)) {
-      return;
-    }
-    prometheusServer.close();
-    logger.info("Prometheus server stopped.");
+  @Bean
+  @ConditionalOnBean(PrometheusHttpServer.class)
+  public SdkMeterProvider sdkMeterProvider(
+      Resource otelResource, PrometheusHttpServer prometheusHttpServer) {
+    return SdkMeterProvider.builder()
+        .addResource(otelResource)
+        .registerMetricReader(prometheusHttpServer)
+        .build();
   }
 }
