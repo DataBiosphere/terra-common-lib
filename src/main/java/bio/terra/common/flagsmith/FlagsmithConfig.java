eab35b5d408329aa8a79cbd59792b8cea8cbdb8c
cahrens <roberts@broadinstitute.org>
2024-02-27 12:55:56 -0500

    Remove flagsmith (PF-2988).

--- src/main/java/bio/terra/common/flagsmith/FlagsmithConfig.java
+++
@@ -1,8 +0,0 @@
-package bio.terra.common.flagsmith;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(value = {FlagsmithProperties.class})
-public class FlagsmithConfig {}
