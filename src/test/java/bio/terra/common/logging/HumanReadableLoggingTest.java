f91a921667a0c6936647b34b3069ad27d5fb4882
David An <davidan@broadinstitute.org>
2024-12-11 12:52:39 -0500

    spotless

--- src/test/java/bio/terra/common/logging/HumanReadableLoggingTest.java
+++ src/test/java/bio/terra/common/logging/HumanReadableLoggingTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
