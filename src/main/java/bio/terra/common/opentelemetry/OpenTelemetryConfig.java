9376141009aa3f9188c703f73aebd7d06eaccf5a
David An <davidan@broadinstitute.org>
2025-03-21 12:09:55 -0400

    CORE-376: disable default otlp trace exports (#229)

--- src/main/java/bio/terra/common/opentelemetry/OpenTelemetryConfig.java
+++ src/main/java/bio/terra/common/opentelemetry/OpenTelemetryConfig.java
@@ -33,8 +33,10 @@ public class OpenTelemetryConfig {
       // the default exporter is the OTLP exporter, which we don't use and outputs errors like:
       // ERROR [OkHttp http://localhost:4318/...] i.o.e.internal.http.HttpExporter: Failed to export
       // metrics. The request could not be executed. Full error message: Failed to connect to
-      // localhost/[0:0:0:0:0:0:0:1]:4318
-      customizer.addPropertiesCustomizer((unused) -> Map.of("otel.metrics.exporter", "none"));
+      // localhost/[0:0:0:0:0:0:0:1]:4318.
+      // The error message may also be "Failed to export spans"
+      customizer.addPropertiesCustomizer(
+          (unused) -> Map.of("otel.metrics.exporter", "none", "otel.traces.exporter", "none"));
 
       customizer.addMeterProviderCustomizer(
           (builder, unused) -> {
